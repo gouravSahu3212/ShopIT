b3d79916f0c903f110d7684941a72c4f25318d62
Gourav <yosahugourav@gmail.com>
2023-10-27 20:40:16 +0530

    code enhancement

--- product/models.py
+++ product/models.py
@@ -6,12 +6,29 @@ from django.utils.text import slugify
 
 
 class Product(models.Model):
+    """
+    A class representing a product.
+
+    Attributes:
+        title (str): The title of the product.
+        slug (str): The slug of the product, used for generating a unique URL.
+        description (str): The description of the product.
+        price (decimal.Decimal): The price of the product.
+        stock (int): The stock quantity of the product.
+        image (django.db.models.ImageField): The image of the product.
+        created_at (django.db.models.DateTimeField): The date and time the product was created.
+        updated_at (django.db.models.DateTimeField): The date and time the product was last updated.
+    """
+
     title = models.CharField(max_length=100)
     slug = models.SlugField(unique=True)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.PositiveIntegerField()
-    image = models.ImageField(upload_to=os.path.join(settings.STATIC_ROOT, 'products'), null=True, blank=True)
+    image = models.ImageField(
+        upload_to=os.path.join(settings.STATIC_ROOT, 'products'),
+        null=True, blank=True
+    )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
